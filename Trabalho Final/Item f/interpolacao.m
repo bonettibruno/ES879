f97137752f1b1d328fcce658e309506847b6cbf5
bruno <brunombonetti@hotmail.com>
2023-12-01 18:48:29 -0300

    item f

---
+++ Trabalho Final/Item f/interpolacao.m
@@ -0,0 +1,53 @@
+clc;
+clear all;
+close all;
+
+[y, Fs] = audioread("Viva la vida - coldplay.wav");
+y = y(:, 1) + y(:, 2);
+
+% Calculando a média do sinal
+ymedio = mean(y);
+
+% Subtraindo a média de cada elemento do sinal
+% Pois: "Se a média do sinal não for zero, irá aparecer uma componente na frequência
+% 0, correspondente ao ganho estático"
+y = y - ymedio;
+
+% Aumentar a taxa de amostragem por um fator de L = 4
+L = 4;
+y_superamostrado = interp(y, L);
+
+t_superamostrado = linspace(0, length(y_superamostrado)/Fs, length(y_superamostrado));
+
+% Plot no domínio do tempo para o sinal original e superamostrado
+figure;
+subplot(2,1,1);
+plot(linspace(0, length(y)/Fs, length(y)), y, 'b', 'LineWidth', 1.5);
+hold on;
+plot(t_superamostrado, y_superamostrado, 'r', 'LineWidth', 1.5);
+title("Sinal Original e Superamostrado - Domínio do Tempo");
+xlabel("t (s)");
+ylabel("Amplitude");
+legend('Sinal Original', 'Sinal Superamostrado');
+
+%Dominio da Frequencia para o Sinal Original
+Y_original = 2 * fft(y);
+frequencies_original = linspace(0, Fs, length(Y_original));
+
+% Domínio da Frequência para o Sinal Superamostrado
+Y_superamostrado = 2 * fft(y_superamostrado);
+frequencies_superamostrado = linspace(0, Fs*L, length(Y_superamostrado));
+
+% Plote os espectros de magnitude do sinal original e do sinal superamostrado
+subplot(2,1,2);
+plot(frequencies_original(1:length(frequencies_original)/2), abs(Y_original(1:length(Y_original)/2)), 'b', 'LineWidth', 1.5);
+hold on;
+plot(frequencies_superamostrado(1:length(frequencies_superamostrado)/2), abs(Y_superamostrado(1:length(Y_superamostrado)/2)), 'r', 'LineWidth', 1.5);
+xlabel("Frequência (Hz)");
+ylabel("Magnitude");
+title("Sinal Original e Sinal Superamostrado - Domínio da Frequência");
+legend('Sinal Original', 'Sinal Superamostrado');
+xlim([0, Fs*L/2]);
+
+%soundsc(y, Fs)
+%soundsc(y_superamostrado, Fs*L);
